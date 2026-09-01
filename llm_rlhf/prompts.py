345dcaf175d29834eb7065de1c75aea4058d1bd5
Mazennaji <mazen.naji.1@gmail.com>
2026-09-01 11:16:43 +0300

    feat(prompts): add initial set of prompts for text generation

---
+++ llm_rlhf/prompts.py
@@ -0,0 +1,10 @@
+PROMPTS = [
+    "The best way to spend a weekend is",
+    "My favorite thing about programming is",
+    "The future of technology will",
+    "When I think about happiness, I",
+    "The most important skill to learn is",
+    "A good leader should always",
+    "The key to a healthy life is",
+    "If I could travel anywhere, I would",
+]
