8e2716577d226cc3221c476d6304afea3174592b
Mazennaji <mazen.naji.1@gmail.com>
2026-09-01 09:57:27 +0300

    feat(reward): enhance RLHFRewardWrapper with dynamic KL coefficient and step tracking

--- rlhf_finetune/reward_wrapper.py
+++ rlhf_finetune/reward_wrapper.py
@@ -1,6 +1,5 @@
 import gymnasium as gym
 import torch
-import numpy as np
 
 
 class ModelRef:
@@ -10,19 +9,27 @@ class ModelRef:
 
 class RLHFRewardWrapper(gym.Wrapper):
     def __init__(self, env, reward_model, base_model, current_model_ref: ModelRef,
-                 action_dim: int, kl_coef: float = 0.1):
+                 action_dim: int, kl_coef: float = 0.1, kl_coef_fn=None, total_steps=None):
         super().__init__(env)
         self.reward_model = reward_model
         self.base_model = base_model
         self.current_model_ref = current_model_ref
         self.action_dim = action_dim
         self.kl_coef = kl_coef
+        self.kl_coef_fn = kl_coef_fn
+        self.total_steps = total_steps
+        self.step_count = 0
         self._last_obs = None
 
     def reset(self, **kwargs):
         obs, info = self.env.reset(**kwargs)
         self._last_obs = obs
         return obs, info
+
+    def _current_kl_coef(self):
+        if self.kl_coef_fn is not None:
+            return self.kl_coef_fn(self.step_count)
+        return self.kl_coef
 
     def step(self, action):
         next_obs, env_reward, terminated, truncated, info = self.env.step(action)
@@ -46,11 +53,14 @@ class RLHFRewardWrapper(gym.Wrapper):
                     current_dist.distribution, base_dist.distribution
                 ).mean().item()
 
-        shaped_reward = learned_reward - self.kl_coef * kl_penalty
+        kl_coef = self._current_kl_coef()
+        shaped_reward = learned_reward - kl_coef * kl_penalty
 
         info["env_reward"] = env_reward
         info["learned_reward"] = learned_reward
         info["kl_penalty"] = kl_penalty
+        info["kl_coef"] = kl_coef
 
+        self.step_count += 1
         self._last_obs = next_obs
         return next_obs, shaped_reward, terminated, truncated, info
