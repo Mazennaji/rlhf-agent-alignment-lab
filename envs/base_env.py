c87626fbef63f76e653d592eaa57bc5df0ddac5c
Mazennaji <mazen.naji.1@gmail.com>
2026-09-01 10:03:49 +0300

    feat(env): add render mode option to environment creation function

--- envs/base_env.py
+++ envs/base_env.py
@@ -1,6 +1,6 @@
 import gymnasium as gym
 
-def make_env(env_id: str = "LunarLander-v3", seed: int = 0):
-    env = gym.make(env_id)
+def make_env(env_id: str = "LunarLander-v3", seed: int = 0, render_mode: str = None):
+    env = gym.make(env_id, render_mode=render_mode) if render_mode else gym.make(env_id)
     env.reset(seed=seed)
     return env
